59f8f48f1c32cdc5526897a6befb7b9e2e3efce3
Stella-055 <stellaongayo243@gmail.com>
2025-05-25 21:29:18 +0300

    added clerk authentication

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 import { Button } from '@/components/ui/button'
+
 const Header = () => {
+  
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'> 
       <Image src={"./logo.svg"} width={160} height={100} alt='logo'/>
-      <Button>Get Started</Button>
+      <Button >Get Started</Button>
     </div>
   )
 }
