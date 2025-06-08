fc5858815fd40c450336a97f610a40d290523bdf
Stella-055 <stellaongayo243@gmail.com>
2025-06-08 15:34:40 +0300

    renamed:    utils/dbConfig.jsx -> db/index.ts 	renamed:    utils/schema.jsx -> db/schema.ts 	deleted:    drizzle.config.js 	new file:   drizzle.config.ts 	modified:   package-lock.json 	modified:   package.json

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,10 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-require('dotenv').config();
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
-const sql = neon(process.env.DATABASE_URL);
+console.log('DATABASE_URL:', process.env.NEXT_PUBLIC_DATABASE_URL);
+
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 export const db = drizzle(sql, { schema });
 
 
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 // db/schema.ts
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 export const Budget = pgTable('budgets', {
   id: serial('id').primaryKey(),
