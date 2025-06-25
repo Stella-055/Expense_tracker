c6b805ebe9378ab42dfd5411fc40988c10f4b6d2
Stella-055 <stellaongayo243@gmail.com>
2025-06-25 21:28:12 +0300

    added budget list

--- app/(routes)/dashboard/budget/components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/components/BudgetList.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import CreateBudget from './CreateBudget'
+import { db } from '@/db'
+import { getTableColumns } from 'drizzle-orm'
+import { Expenses, Budget } from '@/db/schema'
+import { sql } from 'drizzle-orm'
+import { eq } from 'drizzle-orm'
+import { useUser } from '@clerk/nextjs'
 
 const BudgetList = () => {
+  const {user} = useUser();
+  useEffect(() => {
+    getBudgetList()
+  }, [user])
+ 
+const getBudgetList = async () => {
+  // This function can be used to fetch the budget list from the database
+const budgets= await db.select({
+
+  ...getTableColumns(Budget),
+  totalSpend:sql `SUM(${Expenses.amount})`.mapWith(Number),
+ totalItems:sql `COUNT(${Expenses.id})`.mapWith(Number)
+
+
+}).from(Budget)
+.leftJoin(Expenses,eq(Budget.id, Expenses.budget))
+.where (eq(Budget.createdBy, user?.primaryEmailAddress?.emailAddress))
+.groupBy(Budget.id)
+
+console.log(budgets);
+
+
+
+}
   return (
     <div className='mt-7'>
         
--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
 // db/schema.ts
 
-import { pgTable, serial, varchar,integer}from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar,integer, numeric}from 'drizzle-orm/pg-core';
 
 export const Budget = pgTable('budgets', {
   id: serial('id').primaryKey(),
@@ -13,7 +13,7 @@ export const Expenses = pgTable('expenses',{
 
   id:serial('id').primaryKey(),
   name:varchar("name").notNull(),
-  amount:varchar("numeric").default("0"),
+  amount:numeric("amount").default("0"),
   budget:integer("budgetId").references(()=> Budget.id),
   createdAt:varchar("createdAt").notNull()
 })
